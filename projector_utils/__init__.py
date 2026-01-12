4ac968bc2ff779ff8abcfd3b187681929bb7cc12
SS <66886825+EarlMilktea@users.noreply.github.com>
2026-01-13 04:50:09 +0900

    :children_crossing: Define package exports

--- projector_utils/__init__.py
+++ projector_utils/__init__.py
@@ -3,3 +3,15 @@
 This package provides NumPy-based helpers for reshaping tensor axes and
 performing common decompositions used in tensor workflows.
 """
+
+from __future__ import annotations
+
+from projector_utils.decomp import tqr, tsvd
+from projector_utils.merge import group, ungroup
+
+__all__ = [
+    "group",
+    "tqr",
+    "tsvd",
+    "ungroup",
+]
