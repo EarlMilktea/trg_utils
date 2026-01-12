39b30a868d344a6f1c6636dea8144f2da82459c5
SS <66886825+EarlMilktea@users.noreply.github.com>
2026-01-13 03:23:56 +0900

    :truck: Rename

--- tests/test_merge.py
+++ tests/test_merge.py
@@ -3,26 +3,26 @@ from __future__ import annotations
 import numpy as np
 import pytest
 
-from projector_utils import legmerge
+from projector_utils import merge
 
 
 class TestGroup:
     def test_group_ng(self) -> None:
         with pytest.raises(ValueError, match=r".*between 1.*"):
-            legmerge.group(np.eye(3), 0)
+            merge.group(np.eye(3), 0)
 
     def test_ungroup_ng(self) -> None:
         with pytest.raises(ValueError, match=r".*3 -> \(2, 2\).*"):
-            legmerge.ungroup(np.eye(3), (2, 2))
+            merge.ungroup(np.eye(3), (2, 2))
 
     @pytest.mark.parametrize("k", [1, 2, 3, 4, 5])
     def test_gug(self, rng: np.random.Generator, k: int) -> None:
         shape = (2, 3, 4, 5, 6)
         arr0 = rng.random(shape)
-        arr1 = legmerge.group(arr0, k)
-        arr2 = legmerge.ungroup(arr1, shape[-k:])
+        arr1 = merge.group(arr0, k)
+        arr2 = merge.ungroup(arr1, shape[-k:])
         np.testing.assert_array_equal(arr0, arr2)
-        arr3 = legmerge.group(arr2, k)
+        arr3 = merge.group(arr2, k)
         np.testing.assert_array_equal(arr1, arr3)
 
     @pytest.mark.parametrize("k", [1, 2, 3, 4, 5])
@@ -30,12 +30,12 @@ class TestGroup:
         shape = (2, 3, 4, 5, 6)
         arr_c = rng.random(shape)
         arr_f = np.asfortranarray(arr_c)
-        np.testing.assert_array_equal(legmerge.group(arr_c, k), legmerge.group(arr_f, k))
+        np.testing.assert_array_equal(merge.group(arr_c, k), merge.group(arr_f, k))
 
     @pytest.mark.parametrize("k", [1, 2, 3, 4, 5])
     def test_upgroup_fortran(self, rng: np.random.Generator, k: int) -> None:
         shape = (2, 3, 4, 5, 6)
-        arr_c = legmerge.group(rng.random(shape), k)
+        arr_c = merge.group(rng.random(shape), k)
         arr_f = np.asfortranarray(arr_c)
         shape_ = shape[-k:]
-        np.testing.assert_array_equal(legmerge.ungroup(arr_c, shape_), legmerge.ungroup(arr_f, shape_))
+        np.testing.assert_array_equal(merge.ungroup(arr_c, shape_), merge.ungroup(arr_f, shape_))
