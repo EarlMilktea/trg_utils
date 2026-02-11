e3fd420709da14e9aeb6d3126499e30d93170aab
SS <66886825+EarlMilktea@users.noreply.github.com>
2026-02-12 03:15:55 +0900

    :wrench: Setup intersphinx

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,15 @@ copyright = f"2025, {author}"  # noqa: A001
 
 release = importlib.metadata.version(project)
 
-extensions: list[str] = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
+extensions: list[str] = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx.ext.intersphinx"]
 templates_path = ["_templates"]
 exclude_patterns: list[str] = []
 
 root_doc = "index"
 html_theme = "furo"
+default_role = "any"
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+}
